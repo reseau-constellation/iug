be4fe33566cc7f3b6ec15bd53cd0b174452e80ce
julienmalard <julien.malard@mail.mcgill.ca>
2022-12-14 19:32:10 +0100

    Mise à jour gabarit

--- packages/main/vite.config.js
+++ packages/main/vite.config.js
@@ -1,7 +1,9 @@
 import {node} from '../../.electron-vendors.cache.json';
-import {join} from 'path';
+import {join} from 'node:path';
+import {injectAppVersion} from '../../version/inject-app-version-plugin.mjs';
 
 const PACKAGE_ROOT = __dirname;
+const PROJECT_ROOT = join(PACKAGE_ROOT, '../..');
 
 /**
  * @type {import('vite').UserConfig}
@@ -10,7 +12,7 @@ const PACKAGE_ROOT = __dirname;
 const config = {
   mode: process.env.MODE,
   root: PACKAGE_ROOT,
-  envDir: process.cwd(),
+  envDir: PROJECT_ROOT,
   resolve: {
     alias: {
       '/@/': join(PACKAGE_ROOT, 'src') + '/',
@@ -31,11 +33,12 @@ const config = {
       output: {
         entryFileNames: '[name].cjs',
       },
-      external: ['assert', 'wrtc', 'chokidar'],
+      external: ['wrtc'],
     },
     emptyOutDir: true,
     reportCompressedSize: false,
   },
+  plugins: [injectAppVersion()],
 };
 
 export default config;
