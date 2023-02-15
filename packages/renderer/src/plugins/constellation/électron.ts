6978858fc1bc2846defa088591bdae1e4357aef3
julienmalard <julien.malard@mail.mcgill.ca>
2023-02-15 16:29:01 +0100

    Opportunité de débogage

--- packages/renderer/src/plugins/constellation/électron.ts
+++ packages/renderer/src/plugins/constellation/électron.ts
@@ -10,12 +10,16 @@ import {
   GestionnaireServeur,
 } from '@constl/mandataire-electron-rendu';
 
+const enDéveloppement = process.env.NODE_ENV !== 'production';
+
 export default {
   install: (app: App) => {
     const constl = générerMandataireÉlectronPrincipal({
       envoyerMessageÀConstellation,
       écouterMessagesDeConstellation,
+      journal: enDéveloppement ? console.log : undefined,
     });
+
     app.config.globalProperties.$constl = constl;
     app.provide('constl', constl);
 
