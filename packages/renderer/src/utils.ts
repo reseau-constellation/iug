6a0c728e1c9eef428e11cb239d7bac6a4839f9a6
julienmalard <julien.malard@mail.mcgill.ca>
2023-02-15 16:30:06 +0100

    Fonction copier texte et types pour publications GitHub

--- packages/renderer/src/utils.ts
+++ packages/renderer/src/utils.ts
@@ -1,3 +1,66 @@
 export const ouvrirLien = (lien: string) => {
   window.open(lien); // À faire : tester sous Électron
 };
+
+export async function copier(texte: string): Promise<void> {
+  await navigator.clipboard?.writeText(texte);
+}
+
+export type fichierPublicationGitHub = {
+  url: string;
+  browser_download_url: string;
+  id: number;
+  node_id: string;
+  name: string;
+  label: string;
+  state: string;
+  content_type: string;
+  size: number;
+  download_count: number;
+  created_at: number;
+  updated_at: number;
+  uploader: auteurGitHub;
+};
+
+export type auteurGitHub = {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+};
+
+export type publicationGitHub = {
+  url: string;
+  html_url: string;
+  assets_url: string;
+  upload_url: string;
+  tarball_url: string;
+  zipball_url: string;
+  discussion_url: string;
+  id: number;
+  node_id: string;
+  tag_name: string;
+  target_commitish: string;
+  name: string;
+  body: string;
+  draft: boolean;
+  prerelease: boolean;
+  created_at: string;
+  published_at: string;
+  author: auteurGitHub;
+  assets: fichierPublicationGitHub[];
+};
