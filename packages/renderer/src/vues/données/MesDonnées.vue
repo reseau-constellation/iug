adfdceaaf74a7a8075a833d8a6c524c75f4d420c
julienmalard <julien.malard@mail.mcgill.ca>
2023-02-24 18:43:32 +0100

    Nouvelles composantes

--- packages/renderer/src/vues/données/MesDonnées.vue
+++ packages/renderer/src/vues/données/MesDonnées.vue
@@ -1,1 +1,29 @@
-<template><h1></h1></template>
+<template>
+  <TitrePage
+    :titre="$t('pages.compte.titre')"
+    :image="imgDonnées"
+  />
+  <v-row>
+    <v-chip>Mots-clefs</v-chip>
+    <v-chip>Variables</v-chip>
+    <v-chip>Bases de données</v-chip>
+    <v-chip>Projets</v-chip>
+    <v-chip>Nuées</v-chip>
+  </v-row>
+  <v-window>
+    <v-window-item></v-window-item>
+    <v-window-item></v-window-item>
+    <v-window-item></v-window-item>
+    <v-window-item></v-window-item>
+    <v-window-item></v-window-item>
+  </v-window>
+</template>
+
+<script setup lang="ts">
+import TitrePage from '/@/components/TitrePage.vue';
+import {utiliserImagesDéco} from '/@/composables/images';
+
+const {obtImageDéco} = utiliserImagesDéco();
+
+const imgDonnées = obtImageDéco('données');
+</script>
