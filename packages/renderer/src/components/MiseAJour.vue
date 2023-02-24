22498e92f58e992c123d6c14f40e8923e4ee31c5
julienmalard <julien.malard@mail.mcgill.ca>
2023-02-24 16:29:05 +0100

    Création initiale composantes

---
+++ packages/renderer/src/components/MiseAJour.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-alert
+    type="success"
+    variant="outlined"
+    closable
+  >
+    {{
+      $t('avertissements.miseÀJour.texte', {
+        versionCourante: versionAppliFormattée,
+        nouvelleVersion: nouvelleVersionFormattée,
+      })
+    }}
+    <v-btn
+      class="mx-2 my-2"
+      variant="outlined"
+      color="primary"
+      @click="ouvrirLien(urlTéléchargement)"
+    >
+      {{ $t('avertissements.miseÀJour.télécharcher') }}
+      <v-icon end>mdi-download</v-icon>
+    </v-btn>
+  </v-alert>
+</template>
+
+<script setup lang="ts">
+import {onMounted, ref, computed} from 'vue';
+
+import axios from 'axios';
+import semver from 'semver';
+import {surLinux, surMac, surWindows} from '#preload';
+
+import {ouvrirLien, type publicationGitHub} from '/@/utils';
+import {IPA_TÉLÉCHARGEMENTS, URL_TÉLÉCHARGEMENTS} from '/@/consts';
+import {utiliserNumération} from '/@/plugins/localisation/localisation';
+
+const extentionCompatible = (ext: string): boolean => {
+  if (surLinux) {
+    return ext === 'AppImage';
+  } else if (surMac) {
+    return ext === 'dmg';
+  } else if (surWindows) {
+    return ext === 'exe';
+  } else {
+    return false;
+  }
+};
+
+const VERSION_APPLI = import.meta.env.VITE_APP_VERSION;
+
+const {formatterVersion} = utiliserNumération();
+const versionAppliFormattée = formatterVersion(VERSION_APPLI);
+
+const versionPlusRécente = ref<string>();
+const nouvelleVersionFormattée = computed(() => {
+  if (versionPlusRécente.value) return formatterVersion(versionPlusRécente.value);
+  return undefined;
+});
+
+const urlTéléchargement = ref(URL_TÉLÉCHARGEMENTS);
+
+onMounted(async () => {
+  const jsonTéléchargements = (await axios.get(IPA_TÉLÉCHARGEMENTS)).data as publicationGitHub[];
+  for (const t of jsonTéléchargements) {
+    const version = t.name;
+    if (semver.gt(version, VERSION_APPLI)) {
+      for (const fichier of t.assets) {
+        const ext = fichier.name.split('.').pop();
+        if (ext && extentionCompatible(ext)) {
+          versionPlusRécente.value = version;
+          urlTéléchargement.value = fichier.browser_download_url;
+        }
+      }
+    }
+  }
+});
+</script>
---
+++ packages/renderer/src/components/automatisations/ItemAutomatisation.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-list-item>
+    <template #prepend>
+      <v-icon>{{ spécification.type === 'exportation' ? 'mdi-export' : 'mdi-import' }}</v-icon>
+    </template>
+    <v-list-item-title>
+      <v-icon>icôneTypeObjet</v-icon>
+    </v-list-item-title>
+    <v-chip>
+      <v-icon></v-icon>
+      {{ texteJetonÉtat }}
+    </v-chip>
+  </v-list-item>
+</template>
+<script setup lang="ts">
+import type {automatisation} from '@constl/ipa';
+import {computed, ref, onMounted, onUnmounted} from 'vue';
+import {கிளிமூக்கை_உபயோகி} from '/@/plugins/kilimukku/kilimukku-vue';
+
+const {useI18n} = கிளிமூக்கை_உபயோகி();
+const {$t} = useI18n();
+
+const props = defineProps<{
+  spécification: automatisation.SpécificationAutomatisation;
+  état: automatisation.ÉtatAutomatisation;
+}>();
+
+// Chronomètre
+const maintenant = ref(new Date().getTime());
+let oublierChronomètre: number | undefined;
+onMounted(() => {
+  oublierChronomètre = window.setInterval(() => (maintenant.value = new Date().getTime()), 1000);
+});
+onUnmounted(() => {
+  if (oublierChronomètre) clearInterval(oublierChronomètre);
+});
+
+// État automatisation
+const texteJetonÉtat = computed(() => {
+  switch (props.état.type) {
+    case 'écoute':
+      return $t('auto.jetonÉtat.écoute');
+    case 'sync':
+      return $t('auto.jetonÉtat.sync');
+    case 'programmée':
+      return $t('auto.jetonÉtat.programmée', {dans: props.état.à - maintenant.value});
+    case 'erreur':
+      if (props.état.prochaineProgramméeÀ) {
+        return $t('auto.jetonÉtat.erreurRéssayer', {
+          dans: props.état.prochaineProgramméeÀ - maintenant.value,
+        });
+      } else {
+        return $t('auto.jetonÉtat.erreur');
+      }
+    default:
+      throw new Error(props.état);
+  }
+});
+</script>
