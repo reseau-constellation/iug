d435ea2ced3575aae867422ea339411424015bfb
julienmalard <julien.malard@mail.mcgill.ca>
2023-03-02 13:17:24 +0100

    Nombre variable d'auteurs

--- packages/renderer/src/components/communs/AuteursObjet.vue
+++ packages/renderer/src/components/communs/AuteursObjet.vue
@@ -1,7 +1,7 @@
 <template>
   <SérieJetons
     :items="items"
-    :n-max="3"
+    :n-max="nMax"
   >
     <template #jeton="{idBdCompte}">
       <JetonMembre :compte="idBdCompte as string" />
@@ -16,12 +16,24 @@
 </template>
 <script setup lang="ts">
 import type {infoAuteur} from '@constl/ipa/dist/src/utils';
+
 import {computed} from 'vue';
-import ItemMembre from '../membres/ItemMembre.vue';
-import JetonMembre from '../membres/JetonMembre.vue';
+import type {PropType} from 'vue';
+
+import ItemMembre from '/@/components/membres/ItemMembre.vue';
+import JetonMembre from '/@/components/membres/JetonMembre.vue';
 import SérieJetons from './SérieJetons.vue';
 
-const props = defineProps<{auteurs?: infoAuteur[]}>();
+const props = defineProps({
+  auteurs: {
+    type: Array as PropType<infoAuteur[]>,
+    default: () => [],
+  },
+  nMax: {
+    type: Number,
+    default: 3,
+  },
+});
 
 const items = computed(() => {
   return props.auteurs?.map(a => ({id: a.idBdCompte, ...a}));
