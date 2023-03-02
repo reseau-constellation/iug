dd29c057a639e6585866b94562e23335d9926e68
julienmalard <julien.malard@mail.mcgill.ca>
2023-03-02 13:17:05 +0100

    Résultats recherche

---
+++ packages/renderer/src/components/recherche/RésultatRechercheVariable.vue
@@ -0,0 +1,84 @@
+<template>
+  <v-list-item>
+    <v-list-item-title>
+      <TexteSurlignéRecherche
+        v-if="source === 'nom'"
+        :info="résultat.résultatObjectif.info"
+      />
+      <span v-else>{{ nomTraduit }}</span>
+    </v-list-item-title>
+    <v-list-item-subtitle>
+      <TexteSurlignéRecherche
+        v-if="source === 'descr'"
+        :info="résultat.résultatObjectif.info"
+      />
+      <span v-else>{{ descriptionTraduite }}</span>
+    </v-list-item-subtitle>
+    <AuteursObjet
+      :auteurs="auteurs"
+      :n-max="1"
+    ></AuteursObjet>
+    <jeton-id-objet :id="résultat.id">
+      <TexteSurlignéRecherche
+        v-if="source === 'id'"
+        :info="résultat.résultatObjectif.info"
+      />
+      <span v-else>{{ résultat.id }}</span>
+    </jeton-id-objet>
+  </v-list-item>
+</template>
+  <script setup lang="ts">
+  import type ClientConstellation from '@constl/ipa/dist/src/client';
+  import type {infoAuteur, infoRésultatTexte, résultatRecherche} from '@constl/ipa/dist/src/utils';
+  
+  import {computed, inject, ref} from 'vue';
+  
+  import AuteursObjet from '../communs/AuteursObjet.vue';
+  import JetonIdObjet from '../communs/JetonIdObjet.vue';
+  
+  import {enregistrerÉcoute} from '/@/composables/utils';
+  import {utiliserLangues} from '/@/plugins/localisation/localisation';
+  
+  const props = defineProps<{résultat: résultatRecherche<infoRésultatTexte>}>();
+  
+  const constl = inject<ClientConstellation>('constl');
+  
+  const {traduireNom} = utiliserLangues();
+  
+  // Source résultat
+  const source = computed(() => {
+    return props.résultat.résultatObjectif.de;
+  });
+  
+  // Nom
+  const noms = ref<{[lng: string]: string}>({});
+  const nomTraduit = traduireNom(noms);
+  
+  enregistrerÉcoute(
+    constl?.variables?.suivreNomsVariable({
+      id: props.résultat.id,
+      f: x => (noms.value = x),
+    }),
+  );
+  
+  // Descriptions
+  const descriptions = ref<{[lng: string]: string}>({});
+  const descriptionTraduite = traduireNom(descriptions);
+  
+  enregistrerÉcoute(
+    constl?.variables?.suivreDescrVariable({
+      id: props.résultat.id,
+      f: x => (descriptions.value = x),
+    }),
+  );
+  
+  // Auteurs
+  const auteurs = ref<infoAuteur[]>();
+  enregistrerÉcoute(
+    constl?.réseau?.suivreAuteursVariable({
+      idVariable: props.résultat.id,
+      f: x => (auteurs.value = x),
+    }),
+  );
+  </script>
+  
---
+++ packages/renderer/src/components/recherche/TexteSurlignéRecherche.vue
@@ -0,0 +1,81 @@
+<template>
+  <span>
+    <span v-if="texteProcessé.coupéAvant">{{ t('communs.troisPetitsPoints') }}</span>
+    <span>{{ texteProcessé.texteAvant }}</span>
+    <span class="highlight-yellow">{{ texteProcessé.texteSurligné }}</span>
+    <span>{{ texteProcessé.texteAprès }}</span>
+    <span v-if="texteProcessé.coupéAprès">{{ t('communs.troisPetitsPoints') }}</span>
+  </span>
+</template>
+
+<script setup lang="ts">
+import type {infoRésultatTexte} from '@constl/ipa/dist/src/utils';
+import {computed} from 'vue';
+import {கிளிமூக்கை_உபயோகி} from '/@/plugins/kilimukku/kilimukku-vue';
+
+const props = defineProps<{info: infoRésultatTexte; maxTaille?: number}>();
+
+const {useI18n} = கிளிமூக்கை_உபயோகி();
+const {t} = useI18n();
+
+const TAILLE_MIN_TEXTE_AVANT = 4;
+
+const texteProcessé = computed((): {
+  texteAvant: string;
+  texteSurligné: string;
+  texteAprès: string;
+  coupéAvant: boolean;
+  coupéAprès: boolean;
+} => {
+  const {texte, début, fin} = props.info;
+  const texteDIntérêt = texte.slice(début, fin);
+  const préTexte = texte.slice(0, props.info.début);
+  const postTexte = texte.slice(fin);
+  if (props.maxTaille) {
+    const différenceTaille = props.maxTaille - texteDIntérêt.length;
+    if (différenceTaille >= 0) {
+      return {
+        texteAvant: préTexte,
+        texteSurligné: texteDIntérêt,
+        texteAprès: postTexte,
+        coupéAvant: false,
+        coupéAprès: false,
+      };
+    } else {
+        const tailleTexteSurligné = Math.min(texteDIntérêt.length, props.maxTaille - TAILLE_MIN_TEXTE_AVANT);
+
+        const tailleMaxTexteAvant = Math.max(TAILLE_MIN_TEXTE_AVANT, props.maxTaille - tailleTexteSurligné );
+        const tailleMaxTexteAprès = Math.max(0, props.maxTaille - tailleMaxTexteAvant - tailleTexteSurligné);
+
+        const coupéAprès = (texteDIntérêt.length > tailleTexteSurligné) || (postTexte.length > tailleMaxTexteAprès);
+        
+        return {
+            texteAvant: préTexte.slice(0, tailleMaxTexteAvant),
+            texteSurligné: texteDIntérêt.slice(0, tailleTexteSurligné),
+            texteAprès: postTexte.slice(0, tailleMaxTexteAprès),
+            coupéAvant: préTexte.length > tailleMaxTexteAvant,
+            coupéAprès,
+        };
+    }
+  } else {
+    return {
+      texteAvant: préTexte,
+      texteSurligné: texteDIntérêt,
+      texteAprès: postTexte,
+      coupéAvant: false,
+      coupéAprès: false,
+    };
+  }
+});
+</script>
+<style>
+.highlight-yellow {
+  border-radius: 1em 0 1em 0;
+  background-image: linear-gradient(
+    -100deg,
+    rgba(255, 224, 0, 0.2),
+    rgba(255, 224, 0, 0.7) 95%,
+    rgba(255, 224, 0, 0.1)
+  );
+}
+</style>
