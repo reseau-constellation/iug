8189ed7e3008d9b94e5393f3df4170715cd3a331
julienmalard <julien.malard@mail.mcgill.ca>
2023-03-09 16:38:37 +0530

    Jeton exportation et début carte automatisation

---
+++ packages/renderer/src/components/automatisations/CarteAutomatisation.vue
@@ -0,0 +1,97 @@
+<template>
+  <v-card>
+    <v-card-item>
+      <v-card-title>{{ nom }}</v-card-title>
+    </v-card-item>
+    <v-card-text>
+      <division-carte :titre="t('automatisations.statut')" />
+      <jeton-statut-automatisation :statut="statut" />
+      <jeton-fichier-importation
+        v-if="spécification.type === 'importation'"
+        :spécification="spécification"
+      />
+      <jeton-fichier-exportation
+        v-else
+        :spécification="spécification"
+      /> 
+    </v-card-text>
+  </v-card>
+</template>
+<script setup lang="ts">
+import type {automatisation} from '@constl/ipa';
+import type ClientConstellation from '@constl/ipa/dist/src/client';
+
+import {ref, onMounted, inject} from 'vue';
+import DivisionCarte from '../communs/DivisionCarte.vue';
+import JetonFichierExportation from './JetonFichierExportation.vue';
+import JetonFichierImportation from './JetonFichierImportation.vue';
+import JetonStatutAutomatisation from './JetonStatutAutomatisation.vue';
+import {enregistrerÉcoute} from '/@/composables/utils';
+import {கிளிமூக்கை_உபயோகி} from '/@/plugins/kilimukku/kilimukku-vue';
+import {utiliserLangues} from '/@/plugins/localisation/localisation';
+
+const {useI18n} = கிளிமூக்கை_உபயோகி();
+const {t} = useI18n();
+
+const props = defineProps<{
+  spécification: automatisation.SpécificationAutomatisation;
+  statut: automatisation.ÉtatAutomatisation;
+}>();
+
+const constl = inject<ClientConstellation>('constl');
+
+// Nom
+const {traduireNom} = utiliserLangues();
+
+const noms = ref<{[langue: string]: string}>({});
+
+onMounted(() => {
+  if (props.spécification.type === 'importation') {
+    enregistrerÉcoute(
+      constl?.tableaux?.suivreNomsTableau({
+        idTableau: props.spécification.idTableau,
+        f: x => (noms.value = x),
+      }),
+    );
+  } else {
+    switch (props.spécification.typeObjet) {
+      case 'tableau':
+        enregistrerÉcoute(
+          constl?.tableaux?.suivreNomsTableau({
+            idTableau: props.spécification.idObjet,
+            f: x => (noms.value = x),
+          }),
+        );
+        break;
+      case 'projet':
+        enregistrerÉcoute(
+          constl?.projets?.suivreNomsProjet({
+            id: props.spécification.idObjet,
+            f: x => (noms.value = x),
+          }),
+        );
+        break;
+      case 'bd':
+        enregistrerÉcoute(
+          constl?.bds?.suivreNomsBd({
+            id: props.spécification.idObjet,
+            f: x => (noms.value = x),
+          }),
+        );
+        break;
+      case 'nuée':
+        enregistrerÉcoute(
+          constl?.nuées?.suivreNomsNuée({
+            idNuée: props.spécification.idObjet,
+            f: x => (noms.value = x),
+          }),
+        );
+        break;
+      default:
+        throw new Error(JSON.stringify(props.spécification));
+    }
+  }
+});
+
+const nom = traduireNom(noms);
+</script>
---
+++ packages/renderer/src/components/automatisations/JetonFichierExportation.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-chip>
+    <template #prepend>
+      <v-progress-circular
+        v-if="enProgrès"
+        indeterminate
+        color="primary"
+      >
+      </v-progress-circular>
+      <v-icon v-else-if="fichierRésolu">mdi-file</v-icon>
+      <v-icon
+        v-else
+        color="error"
+      >
+        mdi-alert
+      </v-icon>
+    </template>
+    {{ texteStatut }}
+  </v-chip>
+</template>
+
+<script setup lang="ts">
+import type {automatisation} from '@constl/ipa';
+import type ClientConstellation from '@constl/ipa/dist/src/client';
+import path from 'path';
+import {computed, ref, inject, onMounted, watchEffect} from 'vue';
+import {கிளிமூக்கை_உபயோகி} from '/@/plugins/kilimukku/kilimukku-vue';
+
+const props = defineProps<{
+  spécification: automatisation.SpécificationExporter;
+}>();
+
+const {useI18n} = கிளிமூக்கை_உபயோகி();
+const {t} = useI18n();
+const constl = inject<ClientConstellation>('constl');
+const dispositifPrésent = ref<string>();
+
+// Dispositifs
+onMounted(async ()=>{
+  dispositifPrésent.value = await constl?.obtIdOrbite();
+});
+const surCeDispositif = computed<boolean>(()=>{
+  return !!dispositifPrésent.value && props.spécification.dispositifs.includes(dispositifPrésent.value);
+});
+
+// Fichier décodé
+const fichier = ref<string | null>();
+const enProgrès = computed<boolean>(() => {
+  return surCeDispositif.value && fichier.value === undefined;
+});
+watchEffect(async () => {
+  fichier.value = await constl?.automatisations?.résoudreAdressePrivéeFichier({
+      clef: props.spécification.dossier,
+    });
+  },
+);
+const fichierRésolu = computed(()=>{
+  return typeof fichier.value === 'string';
+});
+
+// Texte statut
+const texteStatut = computed<string>(() => {
+  if (fichier.value === undefined) {
+    return t('automatisation.rechercheFichier');
+  } else if (fichier.value === null) {
+    return t('automatisations.fichierIntrouvable');
+  } else {
+    return path.parse(fichier.value).base;
+  }
+});
+</script>
