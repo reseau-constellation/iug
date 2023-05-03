36d46754c890dd8fd91d233d4805a2d60ee2d83c
julienmalard <julien.malard@mail.mcgill.ca>
2023-05-03 11:26:48 -0400

    Possibilité recherche

--- packages/renderer/src/vues/données/MesDonnées.vue
+++ packages/renderer/src/vues/données/MesDonnées.vue
@@ -4,196 +4,209 @@
       :titre="t('pages.mesDonnées.titre')"
       :image="imgDonnées"
     />
-    <v-chip-group
-      v-model="typeDonnées"
-      color="primary"
-      mandatory
-    >
-      <v-chip
-        v-for="item in itemsTypesDonnées"
-        :key="item.clef"
-        class="mx-2"
-        variant="outlined"
-        :prepend-icon="item.icône"
-      >
-        {{ item.texte }}
-      </v-chip>
-    </v-chip-group>
+    <v-container class="pt-0">
+      <v-card variant="flat">
+        <v-card-text class="pt-0">
+          <v-text-field
+            v-model="texteRecherche"
+            variant="outlined"
+            prepend-inner-icon="mdi-magnify"
+          />
+          <v-chip-group
+            v-model="typeDonnées"
+            color="primary"
+            mandatory
+          >
+            <v-chip
+              v-for="item in itemsTypesDonnées"
+              :key="item.clef"
+              class="mx-2"
+              variant="outlined"
+              :prepend-icon="item.icône"
+            >
+              {{ item.texte }}
+            </v-chip>
+          </v-chip-group>
 
-    <v-window v-model="typeDonnées">
-      <v-window-item :key="0">
-        <v-card>
-          <v-card-text>
-            <v-list class="text-start">
-              <NouveauMotClef>
-                <template #activator="{props}">
-                  <v-list-item v-bind="props">
-                    <template #prepend>
-                      <v-icon>mdi-plus</v-icon>
-                    </template>
-                    <v-list-item-title>
-                      {{ t('pages.mesDonnées.nouveauMotClef') }}
-                    </v-list-item-title>
-                  </v-list-item>
-                  <v-divider class="mb-2" />
-                </template>
-              </NouveauMotClef>
-              <CarteMotClef
-                v-for="motClef in mesMotsClefs"
-                :id="motClef"
-                :key="motClef"
-              >
-                <template #activator="{props}">
-                  <ItemMotClef
-                    v-bind="props"
-                    :id="motClef"
-                  />
-                </template>
-              </CarteMotClef>
-            </v-list>
-          </v-card-text>
-        </v-card>
-      </v-window-item>
-      <v-window-item :key="1">
-        <v-card>
-          <v-card-text>
-            <v-list class="text-start">
-              <NouvelleVariable>
-                <template #activator="{props}">
-                  <v-list-item v-bind="props">
-                    <template #prepend>
-                      <v-icon>mdi-plus</v-icon>
-                    </template>
-                    <v-list-item-title>
-                      {{ t('pages.mesDonnées.nouvelleVariable') }}
-                    </v-list-item-title>
-                  </v-list-item>
-                  <v-divider />
-                </template>
-              </NouvelleVariable>
-              <CarteVariable
-                v-for="variable in mesVariables"
-                :id="variable"
-                :key="variable"
-              >
-                <template #activator="{props}">
-                  <ItemVariable
-                    v-bind="props"
-                    :id="variable"
-                  />
-                </template>
-              </CarteVariable>
-            </v-list>
-          </v-card-text>
-        </v-card>
-      </v-window-item>
-      <v-window-item :key="2">
-        <v-card>
-          <v-card-text>
-            <v-list class="text-start">
-              <NouvelleBd>
-                <template #activator="{props}">
-                  <v-list-item v-bind="props">
-                    <template #prepend>
-                      <v-icon>mdi-plus</v-icon>
-                    </template>
-                    <v-list-item-title>
-                      {{ t('pages.mesDonnées.nouvelleBd') }}
-                    </v-list-item-title>
-                  </v-list-item>
-                  <v-divider />
-                </template>
-              </NouvelleBd>
-              <CarteBd
-                v-for="bd in mesBds"
-                :id="bd"
-                :key="bd"
-              >
-                <template #activator="{props}">
-                  <ItemBd
-                    v-bind="props"
-                    :id="bd"
-                  />
-                </template>
-              </CarteBd>
-            </v-list>
-          </v-card-text>
-        </v-card>
-      </v-window-item>
-      <v-window-item :key="3">
-        <v-card>
-          <v-card-text>
-            <v-list class="text-start">
-              <NouveauProjet>
-                <template #activator="{props}">
-                  <v-list-item v-bind="props">
-                    <template #prepend>
-                      <v-icon>mdi-plus</v-icon>
-                    </template>
-                    <v-list-item-title>
-                      {{ t('pages.mesDonnées.nouveauProjet') }}
-                    </v-list-item-title>
-                  </v-list-item>
-                  <v-divider />
-                </template>
-              </NouveauProjet>
-              <CarteProjet
-                v-for="projet in mesProjets"
-                :id="projet"
-                :key="projet"
-              >
-                <template #activator="{props}">
-                  <ItemProjet
-                    v-bind="props"
-                    :id="projet"
-                  />
-                </template>
-              </CarteProjet>
-            </v-list>
-          </v-card-text>
-        </v-card>
-      </v-window-item>
-      <v-window-item :key="4">
-        <v-card>
-          <v-card-text>
-            <v-list class="text-start">
-              <NouvelleNuée>
-                <template #activator="{props}">
-                  <v-list-item v-bind="props">
-                    <template #prepend>
-                      <v-icon>mdi-plus</v-icon>
-                    </template>
-                    <v-list-item-title>
-                      {{ t('pages.mesDonnées.nouvelleNuée') }}
-                    </v-list-item-title>
-                  </v-list-item>
-                  <v-divider />
-                </template>
-              </NouvelleNuée>
-              <CarteNuée
-                v-for="nuée in mesNuées"
-                :id="nuée"
-                :key="nuée"
-              >
-                <template #activator="{props}">
-                  <ItemNuée
-                    v-bind="props"
-                    :id="nuée"
-                  />
-                </template>
-              </CarteNuée>
-            </v-list>
-          </v-card-text>
-        </v-card>
-      </v-window-item>
-    </v-window>
+          <v-window v-model="typeDonnées">
+            <v-window-item :key="0">
+              <v-list class="text-start">
+                <NouveauMotClef>
+                  <template #activator="{props}">
+                    <v-list-item v-bind="props">
+                      <template #prepend>
+                        <v-icon>mdi-plus</v-icon>
+                      </template>
+                      <v-list-item-title>
+                        {{ t('pages.mesDonnées.nouveauMotClef') }}
+                      </v-list-item-title>
+                    </v-list-item>
+                    <v-divider class="mb-2" />
+                  </template>
+                </NouveauMotClef>
+                <CarteMotClef
+                  v-for="motClef in mesMotsClefs"
+                  :id="motClef"
+                  :key="motClef"
+                >
+                  <template #activator="{props}">
+                    <ItemMotClef
+                      v-bind="props"
+                      :id="motClef"
+                    />
+                  </template>
+                </CarteMotClef>
+              </v-list>
+            </v-window-item>
+            <v-window-item :key="1">
+              <v-card>
+                <v-card-text>
+                  <v-list class="text-start">
+                    <NouvelleVariable>
+                      <template #activator="{props}">
+                        <v-list-item v-bind="props">
+                          <template #prepend>
+                            <v-icon>mdi-plus</v-icon>
+                          </template>
+                          <v-list-item-title>
+                            {{ t('pages.mesDonnées.nouvelleVariable') }}
+                          </v-list-item-title>
+                        </v-list-item>
+                        <v-divider />
+                      </template>
+                    </NouvelleVariable>
+                    <CarteVariable
+                      v-for="variable in mesVariables"
+                      :id="variable"
+                      :key="variable"
+                    >
+                      <template #activator="{props}">
+                        <ItemVariable
+                          v-bind="props"
+                          :id="variable"
+                        />
+                      </template>
+                    </CarteVariable>
+                  </v-list>
+                </v-card-text>
+              </v-card>
+            </v-window-item>
+            <v-window-item :key="2">
+              <v-card>
+                <v-card-text>
+                  <v-list class="text-start">
+                    <NouvelleBd>
+                      <template #activator="{props}">
+                        <v-list-item v-bind="props">
+                          <template #prepend>
+                            <v-icon>mdi-plus</v-icon>
+                          </template>
+                          <v-list-item-title>
+                            {{ t('pages.mesDonnées.nouvelleBd') }}
+                          </v-list-item-title>
+                        </v-list-item>
+                        <v-divider />
+                      </template>
+                    </NouvelleBd>
+                    <CarteBd
+                      v-for="bd in mesBds"
+                      :id="bd"
+                      :key="bd"
+                    >
+                      <template #activator="{props}">
+                        <ItemBd
+                          v-bind="props"
+                          :id="bd"
+                        />
+                      </template>
+                    </CarteBd>
+                  </v-list>
+                </v-card-text>
+              </v-card>
+            </v-window-item>
+            <v-window-item :key="3">
+              <v-card>
+                <v-card-text>
+                  <v-list class="text-start">
+                    <NouveauProjet>
+                      <template #activator="{props}">
+                        <v-list-item v-bind="props">
+                          <template #prepend>
+                            <v-icon>mdi-plus</v-icon>
+                          </template>
+                          <v-list-item-title>
+                            {{ t('pages.mesDonnées.nouveauProjet') }}
+                          </v-list-item-title>
+                        </v-list-item>
+                        <v-divider />
+                      </template>
+                    </NouveauProjet>
+                    <CarteProjet
+                      v-for="projet in mesProjets"
+                      :id="projet"
+                      :key="projet"
+                    >
+                      <template #activator="{props}">
+                        <ItemProjet
+                          v-bind="props"
+                          :id="projet"
+                        />
+                      </template>
+                    </CarteProjet>
+                  </v-list>
+                </v-card-text>
+              </v-card>
+            </v-window-item>
+            <v-window-item :key="4">
+              <v-card>
+                <v-card-text>
+                  <v-list class="text-start">
+                    <NouvelleNuée>
+                      <template #activator="{props}">
+                        <v-list-item v-bind="props">
+                          <template #prepend>
+                            <v-icon>mdi-plus</v-icon>
+                          </template>
+                          <v-list-item-title>
+                            {{ t('pages.mesDonnées.nouvelleNuée') }}
+                          </v-list-item-title>
+                        </v-list-item>
+                        <v-divider />
+                      </template>
+                    </NouvelleNuée>
+                    <CarteNuée
+                      v-for="nuée in mesNuées"
+                      :id="nuée"
+                      :key="nuée"
+                    >
+                      <template #activator="{props}">
+                        <ItemNuée
+                          v-bind="props"
+                          :id="nuée"
+                        />
+                      </template>
+                    </CarteNuée>
+                  </v-list>
+                </v-card-text>
+              </v-card>
+            </v-window-item>
+          </v-window>
+        </v-card-text>
+      </v-card>
+    </v-container>
   </div>
 </template>
 
 <script setup lang="ts">
-import {ref, inject} from 'vue';
+import {ref, inject, type Ref} from 'vue';
 
 import type ClientConstellation from '@constl/ipa/dist/src/client';
+import type {
+  infoRésultatRecherche,
+  infoRésultatTexte,
+  résultatRecherche,
+} from '@constl/ipa/dist/src/utils';
+
 import {கிளிமூக்கை_உபயோகி} from '/@/plugins/kilimukku/kilimukku-vue';
 import {utiliserImagesDéco} from '/@/composables/images';
 
@@ -204,7 +217,7 @@ import NouvelleNuée from '/@/components/nuées/NouvelleNuée.vue';
 import NouveauProjet from '/@/components/projets/NouveauProjet.vue';
 import NouvelleVariable from '/@/components/variables/NouvelleVariable.vue';
 import ItemMotClef from '/@/components/motsClefs/ItemMotClef.vue';
-import {enregistrerÉcoute} from '/@/composables/utils';
+import {enregistrerRecherche, enregistrerÉcoute} from '/@/composables/utils';
 import CarteMotClef from '/@/components/motsClefs/CarteMotClef.vue';
 import CarteVariable from '/@/components/variables/CarteVariable.vue';
 import ItemVariable from '/@/components/variables/ItemVariable.vue';
@@ -293,4 +306,30 @@ enregistrerÉcoute(
     f: x => (mesNuées.value = x),
   }),
 );
+
+// Recherche
+const texteRecherche = ref<string>();
+const résulatsRecherche =
+  ref<résultatRecherche<infoRésultatTexte | infoRésultatRecherche<infoRésultatTexte>>[]>();
+enregistrerRecherche({
+  requète: texteRecherche,
+  réfRésultat: résulatsRecherche,
+  fRecherche: async ({
+    requète,
+    nOuProfondeur,
+    réfRésultat,
+  }: {
+    requète: string;
+    nOuProfondeur: number;
+    réfRésultat: Ref<
+      résultatRecherche<infoRésultatTexte | infoRésultatRecherche<infoRésultatTexte>>[]
+    >;
+  }) => {
+    return await constl?.recherche?.rechercherBdSelonTexte({
+      texte: requète,
+      f: x => (réfRésultat.value = x),
+      nRésultatsDésirés: nOuProfondeur,
+    });
+  },
+});
 </script>
