2852f5eea6e136a5dce8450e3be91046aab1ef54
julienmalard <julien.malard@mail.mcgill.ca>
2023-05-03 11:27:30 -0400

    Premier travail pages bds et nuées

--- packages/renderer/src/vues/données/PageBD.vue
+++ packages/renderer/src/vues/données/PageBD.vue
@@ -1,1 +1,7 @@
-<template><h1></h1></template>
+<template>
+  <h1>Ici</h1>
+  <p>{{ id }}</p>
+</template>
+<script setup lang="ts">
+defineProps<{id: string}>();
+</script>
--- packages/renderer/src/vues/données/PageNuée.vue
+++ packages/renderer/src/vues/données/PageNuée.vue
@@ -1,6 +1,38 @@
 <template>
   <v-container>
-    <v-btn> Générer code </v-btn>
+    <TitrePage :titre="nomTraduit || couper(id, 50, t('communs.troisPetitsPoints'))" />
+    <p>{{ id }}</p>
+    <ExporterCode :id-nuee="id" />
   </v-container>
 </template>
-<script setup lang="ts"></script>
+
+<script setup lang="ts">
+import type ClientConstellation from '@constl/ipa/dist/src/client';
+
+import {inject, ref} from 'vue';
+import {enregistrerÉcoute} from '/@/composables/utils';
+import {utiliserLangues} from '/@/plugins/localisation/localisation';
+import {couper} from '/@/utils';
+import {கிளிமூக்கை_உபயோகி} from '/@/plugins/kilimukku/kilimukku-vue';
+import ExporterCode from '/@/components/nuées/ExporterCode.vue';
+
+const props = defineProps<{id: string}>();
+
+const constl = inject<ClientConstellation>('constl');
+
+const {useI18n} = கிளிமூக்கை_உபயோகி();
+const {t} = useI18n();
+
+// Nom de la nuée
+const {traduireNom} = utiliserLangues();
+
+const noms = ref<{[lng: string]: string}>({});
+const nomTraduit = traduireNom(noms);
+
+enregistrerÉcoute(
+  constl?.nuées?.suivreNomsNuée({
+    idNuée: props.id,
+    f: x => (noms.value = x),
+  }),
+);
+</script>
