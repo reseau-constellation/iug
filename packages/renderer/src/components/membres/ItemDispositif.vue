d0ba53c784c69eab6656a154732ce508b5290162
julienmalard <julien.malard@mail.mcgill.ca>
2023-07-04 14:01:03 +0200

    Dispositifs

---
+++ packages/renderer/src/components/membres/ItemDispositif.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-list-item>
+    <template #prepend>
+      <v-badge
+        v-if="couleurPointActivité"
+        dot
+        :color="couleurPointActivité"
+        location="bottom end"
+      >
+        <v-icon>{{ icôneDispositif }}</v-icon>
+      </v-badge>
+      <v-icon v-else>{{ icôneDispositif }}</v-icon>
+    </template>
+    <template #title> {{ nomDispositif }} </template>
+    <v-list-item-subtitle>
+      <lien-objet :id="idDispositif" />
+    </v-list-item-subtitle>
+    <slot />
+  </v-list-item>
+</template>
+<script setup lang="ts">
+import {ref, computed, onMounted, onUnmounted} from 'vue';
+
+import LienObjet from '../communs/LienObjet.vue';
+import {obtIcôneDispositifDeType, utiliserNomEtTypeDispositif} from './utils';
+
+const props = defineProps<{idDispositif: string; idCompte?: string; vuA?: number}>();
+
+// Info dispositif
+const {nomDispositif, typeDispositif} = utiliserNomEtTypeDispositif({
+  idDispositif: props.idDispositif,
+  idCompte: props.idCompte,
+});
+
+// Icône
+const icôneDispositif = computed(() => {
+  return obtIcôneDispositifDeType(typeDispositif.value);
+});
+
+// Activité
+const maintenant = ref(Date.now());
+let idIntervale: ReturnType<typeof setInterval>;
+onMounted(() => {
+  idIntervale = setInterval(() => {
+    maintenant.value = Date.now();
+  }, 1000);
+});
+onUnmounted(() => {
+  if (idIntervale) clearInterval(idIntervale);
+});
+const vuDepuis = computed(() => {
+  return props.vuA ? maintenant.value - props.vuA : undefined;
+});
+const couleurPointActivité = computed(() => {
+  if (!vuDepuis.value) return;
+  if (vuDepuis.value <= 1000 * 60 * 5) {
+    return 'success';
+  } else if (vuDepuis.value <= 1000 * 60 * 30) {
+    return 'warning';
+  }
+  return undefined;
+});
+</script>
---
+++ packages/renderer/src/components/membres/JetonDispositif.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-chip :prepend-icon="icôneDispositif">{{ nomDispositif }}</v-chip>
+</template>
+<script setup lang="ts">
+import { computed } from 'vue';
+import { obtIcôneDispositifDeType, utiliserNomEtTypeDispositif } from './utils';
+
+const props = defineProps<{idDispositif: string, idCompte?: string}>();
+
+// Info dispositif
+const {nomDispositif, typeDispositif} = utiliserNomEtTypeDispositif({
+  idDispositif: props.idDispositif,
+  idCompte: props.idCompte,
+});
+
+// Icône
+const icôneDispositif = computed(() => {
+  return obtIcôneDispositifDeType(typeDispositif.value);
+});
+
+</script>
