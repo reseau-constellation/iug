45c90216aafb168af721d47943486556316060c0
julienmalard <julienmalard@users.noreply.github.com>
2023-07-05 12:15:18 +0000

    deploy: b380b38549d4744df07b69cd3ab6a5ba3cf95892

---
+++ configNode-59ba214a.js
@@ -0,0 +1,2 @@
+import{w as t}from"./index-0491f684.js";import{w as s,A as e}from"./const-a6af36b2.js";import{w as a}from"./index-3abaef44.js";import{w as p}from"./index-de489085.js";import o from"wrtc";const r=a({wrtc:o}),n={libp2p:{transports:[p(),t(),r.transport,s({wrtc:o})],peerDiscovery:[r.discovery],addresses:{listen:e}}};export{n as default};
+//# sourceMappingURL=configNode-59ba214a.js.map
--- configTravailleur-29e0f5a3.js
+++
@@ -1,2 +0,0 @@
-import{w as o}from"./index-19e85f20.js";import{w as r}from"./index-2c28520c.js";const p={libp2p:{transports:[o(),r()]}};export{p as default};
-//# sourceMappingURL=configTravailleur-29e0f5a3.js.map
