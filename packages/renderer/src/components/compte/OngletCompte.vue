d88435e16c710953d5b652dd5f4cb8f01a477aee
julienmalard <julien.malard@mail.mcgill.ca>
2023-08-02 15:39:06 +0200

    Rejoindre compte existant

--- packages/renderer/src/components/compte/OngletCompte.vue
+++ packages/renderer/src/components/compte/OngletCompte.vue
@@ -25,7 +25,7 @@
       <p class="px-0 mb-0 text-h6">
         {{ t('pages.compte.compte.optionsAvancées') }}
       </p>
-      
+
       <v-card variant="outlined">
         <v-list>
           <v-list-item>
@@ -55,6 +55,36 @@
                   ></v-btn>
                 </template>
               </ExporterCompte>
+            </template>
+          </v-list-item>
+          <v-divider class="my-2" />
+          <v-list-item>
+            <v-list-item-title>
+              {{ t('pages.compte.compte.titreRejoindreCompte') }}
+            </v-list-item-title>
+            <v-list-item-subtitle>
+              {{ t('pages.compte.compte.sousTitreRejoindreCompte') }}
+            </v-list-item-subtitle>
+            <template #append>
+              <RejoindreCompte>
+                <template #activator="{props: propsActivateur}">
+                  <v-btn
+                    v-if="mdAndUp"
+                    v-bind="propsActivateur"
+                    append-icon="mdi-account-plus-outline"
+                    variant="outlined"
+                  >
+                    {{ t('pages.compte.compte.btnRejoindreCompte') }}
+                  </v-btn>
+                  <v-btn
+                    v-else
+                    v-bind="propsActivateur"
+                    icon="mdi-account-plus-outline"
+                    variant="outlined"
+                    size="small"
+                  ></v-btn>
+                </template>
+              </RejoindreCompte>
             </template>
           </v-list-item>
           <v-divider class="my-2" />
@@ -96,12 +126,13 @@
 import type {client, réseau} from '@constl/ipa';
 
 import {ref, inject} from 'vue';
-import { useDisplay } from 'vuetify';
+import {useDisplay} from 'vuetify';
 
 import {கிளிமூக்கை_உபயோகி} from '/@/plugins/kilimukku/kilimukku-vue';
 
 import ItemDispositif from '/@/components/membres/ItemDispositif.vue';
 import InviterDispositif from './InviterDispositif.vue';
+import RejoindreCompte from './RejoindreCompte.vue';
 import FermerCompte from './FermerCompte.vue';
 import ExporterCompte from './ExporterCompte.vue';
 
@@ -111,7 +142,7 @@ const constl = inject<client.ClientConstellation>('constl');
 
 const {useI18n} = கிளிமூக்கை_உபயோகி();
 const {t} = useI18n();
-const { mdAndUp } = useDisplay();
+const {mdAndUp} = useDisplay();
 
 // Dispositifs
 const dispositifs = ref<string[]>();
---
+++ packages/renderer/src/components/compte/RejoindreCompte.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-dialog v-model="dialogue">
+    <template #activator="{props}">
+      <slot
+        name="activator"
+        v-bind="{props}"
+      ></slot>
+    </template>
+    <v-card
+      class="mx-auto"
+      :max-width="mdAndUp ? 500 : 300"
+    >
+      <v-card-item>
+        <v-card-title class="d-flex">
+          <span class="text-error">
+            {{ t('compte.rejoindreCompte.titre') }}
+          </span>
+          <v-spacer />
+          <v-btn
+            icon="mdi-close"
+            size="small"
+            variant="flat"
+            @click="dialogue = false"
+          ></v-btn>
+        </v-card-title>
+      </v-card-item>
+      <v-divider></v-divider>
+      <v-card-text>
+        <v-window>
+          <v-window-item>
+            <span class="text-error font-weight-bold">{{
+              t('compte.rejoindreCompte.avertissement')
+            }}</span>
+            {{ t('compte.rejoindreCompte.explication') }}
+          </v-window-item>
+          <v-window-item>
+            <v-btn @click="rejoindreCompte" />
+          </v-window-item>
+        </v-window>
+      </v-card-text>
+      <v-divider></v-divider>
+      <v-card-actions> </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+<script setup lang="ts">
+import type {client} from '@constl/ipa';
+
+import {inject, ref} from 'vue';
+import {useDisplay} from 'vuetify';
+import {கிளிமூக்கை_உபயோகி} from '/@/plugins/kilimukku/kilimukku-vue';
+
+const constl = inject<client.ClientConstellation>('constl');
+
+const {mdAndUp} = useDisplay();
+
+const {useI18n} = கிளிமூக்கை_உபயோகி();
+const {t} = useI18n();
+
+// Navigation
+const dialogue = ref(false);
+
+// Rejoindre compte
+const idCompte = ref<string>();
+const codeSecret = ref<string>();
+
+const rejoindreCompte = async () => {
+  if (!idCompte.value || !codeSecret.value) return;
+  await constl?.demanderEtPuisRejoindreCompte({
+    idCompte: idCompte.value,
+    codeSecret: codeSecret.value,
+  });
+};
+</script>
