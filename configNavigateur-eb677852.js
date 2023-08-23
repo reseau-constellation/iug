5acf71bf31f90eef24e642a3cfe720caeb3dce22
julienmalard <julienmalard@users.noreply.github.com>
2023-08-23 17:14:07 +0000

    deploy: 3ec1b10fb2aa556d49df5c1a6b0d534d65ed2172

---
+++ configNavigateur-eb677852.js
@@ -0,0 +1,2 @@
+import{w as r,A as s}from"./const-62df1d20.js";import{w as t}from"./index-b83205e7.js";import"./index-f8f22782.js";import"./index-cdb53a54.js";const i={libp2p:{transports:[r(),t()],addresses:{listen:s}}};export{i as default};
+//# sourceMappingURL=configNavigateur-eb677852.js.map
