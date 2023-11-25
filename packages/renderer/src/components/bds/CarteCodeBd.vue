c7689074b1edd89cb1a498175d21dbb4983a56bf
julienmalard <julien.malard@mail.mcgill.ca>
2023-11-25 12:32:25 +0100

    Progrès

---
+++ packages/renderer/src/components/bds/CarteCodeBd.vue
@@ -0,0 +1,17 @@
+<template>
+  <CarteCode :codes="codes"></CarteCode>
+</template>
+<script setup lang="ts">
+const props = defineProps<{id: string}>();
+
+const codes = {
+    'ts': `
+client.bds.suivreDonnéesExportation({
+
+})
+`,
+    'py': `
+${props.id}
+`,
+};
+</script>
---
+++ packages/renderer/src/components/tableaux/CarteCodeTableau.vue
@@ -0,0 +1,52 @@
+<template>
+  <CarteCode :codes="codes"></CarteCode>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{id: string}>();
+const port = 5000;
+const codes = {
+    'ts': `
+const analyser = async (données) => {
+    console.log(données) // Faire quelque chose de plus intelligent ici :)
+}
+const fOublierDonnées = await client.tableaux.suivreDonnéesExportation({
+    idTableau: ${props.id},
+    langues: ["த", "fr"],
+    f: analyser
+});
+`,
+    'julia': `
+
+`,
+    'R': `
+
+`,
+    'py': `
+import trio
+from constellationPy import ouvrir_client
+
+async def principale():
+    async with ouvrir_client(${port}) as client:
+        données = await client.obt_données_tableau(
+            id_tableau=${props.id}, langues=["த", "fr"], formatDonnées="pandas"
+        )
+        
+        # Analyser ou sauvegarder les données ici...
+        données.to_excel("./MesDonnéesExportées.xlsx")
+
+trio.run(principale)
+`,
+    'py:sync': `
+from constellationPy import ClientSync
+
+client = ClientSync(${port})
+données = client.obt_données_tableau(
+    id_tableau=${props.id}, langues=["த", "fr"], formatDonnées="pandas"
+)
+
+# Analyser ou sauvegarder les données ici...
+données.to_excel("./MesDonnéesExportées.xlsx")
+`,
+};
+</script>
