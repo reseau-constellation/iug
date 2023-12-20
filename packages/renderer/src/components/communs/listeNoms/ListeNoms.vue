25317a6241d312ed7a8725973d383c9c84b0a897
julienmalard <julien.malard@mail.mcgill.ca>
2023-12-20 12:54:51 +0100

    Montrer nombre de langues

--- packages/renderer/src/components/communs/listeNoms/ListeNoms.vue
+++ packages/renderer/src/components/communs/listeNoms/ListeNoms.vue
@@ -57,7 +57,7 @@
   </v-list>
   <span v-show="listeNoms.length">
     <v-divider class="mb-2" />
-    <p class="text-overline">{{ t('communs.autresLangues') }}</p>
+    <p class="text-overline">{{ t('communs.autresLangues', {n: nNomsFormatté}) }}</p>
     <v-list
       :max-height="longue ? 100 : 200"
       style="overflow-y: scroll"
@@ -82,7 +82,7 @@
 <script setup lang="ts">
 import {computed, onMounted, ref, watchEffect} from 'vue';
 import {useDisplay} from 'vuetify';
-import {கிளிமூக்கை_பயன்படுத்து, மொழிகளைப்_பயன்படுத்து} from '@lassi-js/kilimukku-vue';
+import {எண்களைப்_பயன்படுத்து, கிளிமூக்கை_பயன்படுத்து, மொழிகளைப்_பயன்படுத்து} from '@lassi-js/kilimukku-vue';
 
 import {v4 as uuidv4} from 'uuid';
 
@@ -95,9 +95,9 @@ import GestionnaireEnnikkai from '/@/components/langues/contribuer/ennikkai/Gest
 const {கிடைக்கும்_மொழிகளை_பயன்படுத்து} = கிளிமூக்கை_பயன்படுத்து();
 const {மொழிகளும்_குறியீடுகளும், மொழியின்_பெயர்} = கிடைக்கும்_மொழிகளை_பயன்படுத்து({});
 const {மொழியாக்கம்_பயன்படுத்து} = கிளிமூக்கை_பயன்படுத்து();
+const {எண்ணை_வடிவூட்டு} = எண்களைப்_பயன்படுத்து();
 const {$மொ: t} = மொழியாக்கம்_பயன்படுத்து({});
 const {வலதிலிருந்து_இடது_மொழி} = மொழிகளைப்_பயன்படுத்து();
-
 const historiqueLangues = utiliserHistoriqueLangues();
 
 const {mdAndUp} = useDisplay();
@@ -117,6 +117,8 @@ const émettre = defineEmits<{
   (é: 'ajusterNoms', noms: {[lng: string]: string}): void;
 }>();
 
+
+// Noms
 const noms = ref<{[lng: string]: string}>({});
 onMounted(() => {
   noms.value = Object.fromEntries(Object.entries(props.nomsInitiaux));
@@ -138,6 +140,8 @@ watchEffect(() => {
     };
   });
 });
+
+const nNomsFormatté = எண்ணை_வடிவூட்டு(computed(()=>listeNoms.value.length));
 
 // Langues
 const languesDisponibles = computed(() => {
