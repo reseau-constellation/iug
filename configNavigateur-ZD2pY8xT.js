8860ed4159766cd815994157ef0e6fae6705c923
julienmalard <julienmalard@users.noreply.github.com>
2024-01-16 23:12:26 +0000

    deploy: d13e2aef0fff6f4340645f4f665d09ea1655146a

---
+++ configNavigateur-ZD2pY8xT.js
@@ -0,0 +1,2 @@
+import{w as r,A as t}from"./const-V6Nu2FrE.js";import{w as o}from"./index-BQESDX7A.js";import"./index-0JqKiiTm.js";import"./index-IvUeTVj7.js";import"./browser-Lotc6lk-.js";const m={libp2p:{transports:[r(),o()],addresses:{listen:t}}};export{m as default};
+//# sourceMappingURL=configNavigateur-ZD2pY8xT.js.map
