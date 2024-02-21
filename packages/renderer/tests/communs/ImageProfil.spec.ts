4da77b1bb27a4c8e1f42362d289c825380413e2e
julienmalard <julien.malard@mail.mcgill.ca>
2024-02-21 17:42:05 +0100

    Test de base

--- packages/renderer/tests/communs/ImageProfil.spec.ts
+++ packages/renderer/tests/communs/ImageProfil.spec.ts
@@ -4,7 +4,7 @@ import {expect, test} from 'vitest';
 import ImageProfil from '../../src/components/communs/ImageProfil.vue';
 
 import vuetify from '../../src/plugins/vuetify';
-import {constellationTest} from '../utils';
+import {attendreQue, constellationTest} from '../utils';
 import {createPinia} from 'pinia';
 
 const mountFunction = (composante, options?: Record<string, unknown>) => {
@@ -17,42 +17,13 @@ const mountFunction = (composante, options?: Record<string, unknown>) => {
 };
 
 test('Composante ImageProfil - sans id compte', async () => {
-  console.log('ici');
-  expect(ImageProfil).toBeTruthy();
-  console.log('là');
-  const enveloppe = mountFunction(ImageProfil, {
-    props: {},
-  });
-
-  const élImage = enveloppe.get('img');
-
-  expect(élImage.text()).toBe('');
-});
-
-test('Composante TitrePage - sousTitre', async () => {
   expect(ImageProfil).toBeTruthy();
 
-  const titre = 'Mon titre de page';
-  const sousTitre = 'Mon sous-titre de page';
   const enveloppe = mountFunction(ImageProfil, {
-    props: {titre, sousTitre},
+    props: { },
   });
+  await attendreQue(() => !!enveloppe.find('img'));
+  const élImage = enveloppe.find('img');
 
-  const élSousTitre = enveloppe.get('h2');
-
-  expect(élSousTitre.text()).toBe(sousTitre);
-});
-
-test('Composante TitrePage - image', async () => {
-  expect(ImageProfil).toBeTruthy();
-
-  // const titre = 'Mon titre de page';
-  const image = 'urlImage.png';
-  const enveloppe = mountFunction(ImageProfil, {
-    props: {},
-  });
-
-  const élSousTitre = enveloppe.get('img');
-
-  expect(élSousTitre.attributes()['src']).toBe(image);
+  expect(élImage.attributes()['src']).toContain('svg');
 });
