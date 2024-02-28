0d9058a6f79cf0d565a69c3898c8d3e3e6fc1104
julienmalard <julien.malard@mail.mcgill.ca>
2024-02-28 15:15:15 +0100

    Formattage

--- packages/main/src/constellation.ts
+++ packages/main/src/constellation.ts
@@ -6,7 +6,7 @@ const importationIPA = import('@constl/ipa');
 const importationServeur = import('@constl/serveur');
 
 // Avec ça, on peut spécifier le dossier Constellation dans les tests
-const opts = process.env.DOSSIER_CONSTL ? { dossier: process.env.DOSSIER_CONSTL } : undefined;
+const opts = process.env.DOSSIER_CONSTL ? {dossier: process.env.DOSSIER_CONSTL} : undefined;
 
 export const gestionnaireFenêtres = new GestionnaireFenêtres({
   enDéveloppement,
