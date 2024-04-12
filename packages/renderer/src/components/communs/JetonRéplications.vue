d3a69ea7cc6dc2ad33f77cd13515c49c0be90697
julienmalard <julien.malard@mail.mcgill.ca>
2024-04-12 17:12:35 +0200

    Réparer jeton réplications

--- packages/renderer/src/components/communs/JetonRéplications.vue
+++ packages/renderer/src/components/communs/JetonRéplications.vue
@@ -1,22 +1,22 @@
 <template>
   <v-chip
-    class="mb-2 me-2"
     variant="outlined"
     label
   >
-    <v-icon start>mdi-pin-outline</v-icon>
-    {{ nRéplicationsDispositifsFormatté }}
-    {{ t('réplications.réplications', {n: nRéplicationsDispositifsFormatté}, nRéplicationsDispositifs) }}
-    <span v-if="nRéplicationsDispositifs > 0">{{
-      t('réplications.réplicationsEnLigne', [nRéplicationsEnLigneFormatté])
-    }}</span>
+    <v-icon start>{{ icône }}</v-icon>
+    {{ t('réplications.réplications', {n: nRéplicationsDispositifs}, nRéplicationsDispositifs) }}
+    <span
+      v-if="nRéplicationsDispositifs > 0"
+      class="ms-2"
+      >{{ t('réplications.réplicationsEnLigne', [nRéplicationsEnLigne]) }}</span
+    >
   </v-chip>
 </template>
 <script setup lang="ts">
 import {computed} from 'vue';
 import {constellation, suivre} from '/@/components/utils';
 
-import {கிளிமூக்கை_பயன்படுத்து, எண்களைப்_பயன்படுத்து} from '@lassi-js/kilimukku-vue';
+import {கிளிமூக்கை_பயன்படுத்து} from '@lassi-js/kilimukku-vue';
 
 const props = defineProps<{id: string}>();
 
@@ -24,17 +24,24 @@ const constl = constellation();
 
 const {மொழியாக்கம்_பயன்படுத்து} = கிளிமூக்கை_பயன்படுத்து();
 const {$மொ: t} = மொழியாக்கம்_பயன்படுத்து();
-const {எண்ணை_வடிவூட்டு} = எண்களைப்_பயன்படுத்து();
 
 // Réplications
 const réplications = suivre(constl.réseau.suivreRéplications, {idObjet: props.id, profondeur: 5});
 const nRéplicationsDispositifs = computed(() => {
   return réplications.value?.dispositifs.length || 0;
 });
-const nRéplicationsDispositifsFormatté = எண்ணை_வடிவூட்டு(nRéplicationsDispositifs);
 
 const nRéplicationsEnLigne = computed(() => {
   return réplications.value?.dispositifs.filter(d => !d.vuÀ).length || 0;
 });
-const nRéplicationsEnLigneFormatté = எண்ணை_வடிவூட்டு(nRéplicationsEnLigne);
+
+const monCompte = suivre(constl.suivreIdCompte);
+const répliquéParMoi = computed(
+  () => !!réplications.value?.membres.find(m => m.infoMembre.idCompte === monCompte.value),
+);
+
+// Icône
+const icône = computed(() => {
+  return répliquéParMoi.value ? 'mdi-pin' : 'mdi-pin-outline';
+});
 </script>
