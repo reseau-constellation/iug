52f80f2baf1f12bccb4edf0ebcb87596794cc05c
julienmalard <julien.malard@mail.mcgill.ca>
2024-04-15 09:05:33 +0200

    Statut

--- packages/renderer/src/components/communs/ChoisirStatut.vue
+++ packages/renderer/src/components/communs/ChoisirStatut.vue
@@ -1,15 +1,20 @@
 <template>
   <v-select
     v-model="statut"
-    :items="['active', 'obsolète', 'jouet', 'interne']"
+    :items="['active', 'obsolète', 'jouet', 'interne'] as const"
+    variant="outlined"
   >
     <template #item="{item, props: propsItem}">
       <v-list-item
         v-bind="propsItem"
         :title="t(`données.statut.${item.raw}`)"
+        :prepend-icon="icôneStatut(item.raw)"
       >
         {{ t(`données.statut.info.${item.raw}`) }}
       </v-list-item>
+    </template>
+    <template #selection="{item}">
+      {{ t(`données.statut.${item.raw}`) }}
     </template>
   </v-select>
   <v-checkbox
@@ -48,4 +53,21 @@ watchEffect(() => {
     émettre('choisir', {statut: statut.value, idNouvelle: idNouvelle.value});
   }
 });
+
+// Icône
+const icôneStatut = (statut: 'active' | 'obsolète' | 'jouet' | 'interne') => {
+  switch (statut) {
+    case 'active':
+      return 'mdi-check';
+    case 'obsolète':
+      return 'mdi-alert-outline'
+    case 'jouet':
+      return 'mdi-teddy-bear'
+    case 'interne':
+      return 'mdi-xml'
+    default:
+      break;
+  }
+}
+
 </script>
