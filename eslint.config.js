50149d984550d9440e1218e925aa0d627086b046
julienmalard <julien.malard@mail.mcgill.ca>
2024-06-08 08:16:16 +0200

    Mises à jour eslint

--- eslint.config.js
+++ eslint.config.js
@@ -10,6 +10,7 @@ export default [
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+
   ...pluginVue.configs['flat/recommended'],
   {
     files: ['**/*.{js,mjs,cjs,ts,mts,cts,vue}'],
@@ -18,16 +19,13 @@ export default [
         sourceType: 'module',
         parser: '@typescript-eslint/parser',
       },
-      globals: {
-        ...globals.node,
-      },
     },
     ignores: ['**/node_modules/**', '**/dist/**', '**/coverage/**'],
     rules: {
       '@typescript-eslint/no-var-requires': 'off',
       '@typescript-eslint/consistent-type-imports': 'error',
       '@typescript-eslint/no-unused-vars': [
-        'warn',
+        'error',
         {
           argsIgnorePattern: '^_',
           varsIgnorePattern: '^_',
@@ -59,6 +57,51 @@ export default [
       'vue/singleline-html-element-content-newline': 'off',
       /** Désactivée pour incompatibilité internationale*/
       'vue/multi-word-component-names': 'off',
+    },
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    languageOptions: {
+      parserOptions: {
+        sourceType: 'module',
+        parser: '@typescript-eslint/parser',
+      },
+      globals: {
+        ...globals.node,
+      },
+    },
+    ignores: ['**/node_modules/**', '**/dist/**', '**/coverage/**'],
+    rules: {
+      '@typescript-eslint/no-var-requires': 'off',
+      '@typescript-eslint/consistent-type-imports': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+      /**
+       * Having a semicolon helps the optimizer interpret your code correctly.
+       * This avoids rare errors in optimized code.
+       * @see https://twitter.com/alex_kozack/status/1364210394328408066
+       */
+      semi: ['error', 'always'],
+      /**
+       * This will make the history of changes in the hit a little cleaner
+       */
+      'comma-dangle': ['warn', 'always-multiline'],
+      /**
+       * Just for beauty
+       */
+      quotes: [
+        'warn',
+        'single',
+        {
+          avoidEscape: true,
+        },
+      ],
     },
   },
 ];
