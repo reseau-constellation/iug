86de739776dc1381f85129fda643e7aee1e1d079
julienmalard <julien.malard@mail.mcgill.ca>
2024-10-04 16:40:11 +0530

    Gérer erreurs de démarrage

--- packages/renderer/src/components/communs/DialogueErreurInit.vue
+++ packages/renderer/src/components/communs/DialogueErreurInit.vue
@@ -5,33 +5,57 @@
   >
     <v-card class="mx-auto">
       <v-card-text class="text-center">
-        <p class="text-h4">Constellation est déjà ouverte</p>
+        <p class="text-h4">{{ t(titre) }}</p>
         <p class="text-h6">
-          Assurez-vous de fermer toutes les instances de Constellation (ligne de commande, Python
-          ou autre) ouvertes sur votre ordinateur, puis relancez Constellation.
+          {{ t(explication) }}
         </p>
-        <v-btn @click="() => redémarrer()">Relancer Constellation</v-btn>
+        <v-btn @click="() => redémarrer()">{{ 'erreurs.relancer' }}</v-btn>
       </v-card-text>
     </v-card>
   </v-dialog>
 </template>
 <script setup lang="ts">
 import {computed} from 'vue';
-// import { utiliserConstellation} from '/@/components/utils';
+import { utiliserConstellation} from '/@/components/utils';
 import {redémarrer} from '#preload';
 import {
   ERREUR_INIT_IPA,
   ERREUR_INIT_IPA_DÉJÀ_LANCÉ,
-  type ErreurMandataire,
 } from '@constl/mandataire';
-import {ref} from 'vue';
+import { suivre } from '@constl/vue';
+import { கிளிமூக்கை_பயன்படுத்து } from '@lassi-js/kilimukku-vue';
 
-// const constl = utiliserConstellation();
+const {மொழியாக்கம்_பயன்படுத்து} = கிளிமூக்கை_பயன்படுத்து();
+const {$மொ: t} = மொழியாக்கம்_பயன்படுத்து();
 
-const erreursConstellation = ref<ErreurMandataire | undefined>(undefined); // suivre(constl.suivreErreurs);
+const constl = utiliserConstellation();
+
+const erreursConstellation = suivre(constl.suivreErreurs);
 const dialogue = computed(
-  () =>
-    erreursConstellation.value?.code === ERREUR_INIT_IPA ||
-    erreursConstellation.value?.code === ERREUR_INIT_IPA_DÉJÀ_LANCÉ,
+  () => erreurDéjàLancée.value || erreurInit.value,
+);
+
+const erreurDéjàLancée = computed (
+  () => erreursConstellation.value?.code === ERREUR_INIT_IPA_DÉJÀ_LANCÉ,
+);
+
+const erreurInit = computed(
+  () => erreursConstellation.value?.code === ERREUR_INIT_IPA,
+);
+
+const titre = computed(
+  () => {
+    if (erreurDéjàLancée.value) return 'erreurs.init.titre';
+    else if (erreurInit.value) return 'erreurs.déjàLancé.titre';
+    else return '';
+  },
+);
+
+const explication = computed(
+  () => {
+    if (erreurDéjàLancée.value) return 'erreurs.init.explication';
+    else if (erreurInit.value) return 'erreurs.déjàLancé.explication';
+    else return '';
+  },
 );
 </script>
