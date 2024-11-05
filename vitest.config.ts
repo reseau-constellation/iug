932c32a7cb92432751e6e648f5942e6e672228d7
julienmalard <julien.malard@mail.mcgill.ca>
2024-11-05 14:59:43 +0530

    Utiliser ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,5 @@
-/**
- * Configuration for the global end-to-end testing,
- * placed in the project's root 'tests' folder.
- * @type {import('vite').UserConfig}
- * @see https://vitest.dev/config/
- */
-const config = {
+import {defineConfig} from 'vitest/config';
+const config = defineConfig({
   test: {
     /**
      * By default, vitest searches for the test files in all packages.
@@ -18,6 +13,6 @@ const config = {
     testTimeout: 30_000,
     hookTimeout: 30_000,
   },
-};
+});
 
 export default config;
