b51cc51da0d287a5388d14dd4a21700a0d46d3fb
julienmalard <julienmalard@users.noreply.github.com>
2024-11-06 12:15:07 +0000

    deploy: c9182fcf2e44def1d1346dafc04b0c33baf38bd9

---
+++ PageProjet-81TTJQhD.js
@@ -0,0 +1,2 @@
+import{u as s}from"./historiqueDocuments-CwKO0l6R.js";import{d as r,o as a,a as i,N as u,t as c}from"./index-BQ50ekR7.js";const l=r({__name:"PageProjet",props:{id:{}},setup(e){const o=e,t=s();return a(()=>{t.documentOuvert({id:o.id,à:Date.now()})}),(n,m)=>(i(),u("h1",null,c(n.id),1))}});export{l as default};
+//# sourceMappingURL=PageProjet-81TTJQhD.js.map
---
+++ VSlideGroupItem-DJM0jdQb.js
@@ -0,0 +1,2 @@
+import{b7 as o,di as r,dj as p,dk as u}from"./index-BQ50ekR7.js";const d=o()({name:"VSlideGroupItem",props:r(),emits:{"group:selected":s=>!0},setup(s,t){let{slots:l}=t;const e=p(s,u);return()=>l.default?.({isSelected:e.isSelected.value,select:e.select,toggle:e.toggle,selectedClass:e.selectedClass.value})}});export{d as V};
+//# sourceMappingURL=VSlideGroupItem-DJM0jdQb.js.map
