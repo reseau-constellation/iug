44d44a45c9eedb74023c72810ec5a6f2c67a9262
julienmalard <julienmalard@users.noreply.github.com>
2024-11-08 09:09:18 +0000

    deploy: e6eccf1de0406c6f060d5da48663130bc78ebf77

--- PageProjet-DIFUkAeG.js
+++
@@ -1,2 +0,0 @@
-import{u as s}from"./historiqueDocuments-3CB4jZer.js";import{d as r,o as a,a as i,v as u,t as c}from"./index-Dlr5smoe.js";const l=r({__name:"PageProjet",props:{id:{}},setup(e){const o=e,t=s();return a(()=>{t.documentOuvert({id:o.id,à:Date.now()})}),(n,m)=>(i(),u("h1",null,c(n.id),1))}});export{l as default};
-//# sourceMappingURL=PageProjet-DIFUkAeG.js.map
---
+++ PageProjet-npzISvfv.js
@@ -0,0 +1,2 @@
+import{u as s}from"./historiqueDocuments-BMdKPgR7.js";import{d as r,o as a,a as i,v as u,t as c}from"./index-Cl5VbB6W.js";const l=r({__name:"PageProjet",props:{id:{}},setup(e){const o=e,t=s();return a(()=>{t.documentOuvert({id:o.id,à:Date.now()})}),(n,m)=>(i(),u("h1",null,c(n.id),1))}});export{l as default};
+//# sourceMappingURL=PageProjet-npzISvfv.js.map
