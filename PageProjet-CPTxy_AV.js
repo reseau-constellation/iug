f5ca4a6085ec55e06b0f605fe7bb248364a8ccb0
julienmalard <julienmalard@users.noreply.github.com>
2024-11-11 14:13:32 +0000

    deploy: fbade325aa8267c5ff4332189d1b2597b5c86a14

---
+++ PageProjet-CPTxy_AV.js
@@ -0,0 +1,2 @@
+import{u as s}from"./historiqueDocuments-CXddbYs9.js";import{d as r,o as a,a as i,v as u,t as c}from"./index-CtZK4qzp.js";const l=r({__name:"PageProjet",props:{id:{}},setup(e){const o=e,t=s();return a(()=>{t.documentOuvert({id:o.id,à:Date.now()})}),(n,m)=>(i(),u("h1",null,c(n.id),1))}});export{l as default};
+//# sourceMappingURL=PageProjet-CPTxy_AV.js.map
--- PageProjet-DivtP-c1.js
+++
@@ -1,2 +0,0 @@
-import{u as s}from"./historiqueDocuments-EFlVHmFw.js";import{d as r,o as a,a as i,v as u,t as c}from"./index-Dr3-NSis.js";const l=r({__name:"PageProjet",props:{id:{}},setup(e){const o=e,t=s();return a(()=>{t.documentOuvert({id:o.id,à:Date.now()})}),(n,m)=>(i(),u("h1",null,c(n.id),1))}});export{l as default};
-//# sourceMappingURL=PageProjet-DivtP-c1.js.map
