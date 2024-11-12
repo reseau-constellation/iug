27b8f33129bbff80e4f8132faecfc7a78fd1cce1
julienmalard <julienmalard@users.noreply.github.com>
2024-11-12 04:58:46 +0000

    deploy: caced3faa929610aaeedb8110a474b200201e96a

--- PageProjet-CIzxAbBt.js
+++
@@ -1,2 +0,0 @@
-import{u as s}from"./historiqueDocuments-8eWwb1rm.js";import{d as r,o as a,a as i,v as u,t as c}from"./index-Dp4d_G-Y.js";const l=r({__name:"PageProjet",props:{id:{}},setup(e){const o=e,t=s();return a(()=>{t.documentOuvert({id:o.id,à:Date.now()})}),(n,m)=>(i(),u("h1",null,c(n.id),1))}});export{l as default};
-//# sourceMappingURL=PageProjet-CIzxAbBt.js.map
--- VSlideGroupItem-zC1Cxl5f.js
+++
@@ -1,2 +0,0 @@
-import{be as o,dk as r,dl as p,dm as u}from"./index-Dp4d_G-Y.js";const d=o()({name:"VSlideGroupItem",props:r(),emits:{"group:selected":s=>!0},setup(s,t){let{slots:l}=t;const e=p(s,u);return()=>l.default?.({isSelected:e.isSelected.value,select:e.select,toggle:e.toggle,selectedClass:e.selectedClass.value})}});export{d as V};
-//# sourceMappingURL=VSlideGroupItem-zC1Cxl5f.js.map
