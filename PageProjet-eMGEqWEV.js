fba008a3ec56782b93c091c2c2fd82606b4ae3b7
julienmalard <julienmalard@users.noreply.github.com>
2024-11-18 08:58:06 +0000

    deploy: 5623bf16f497f572c269193186d303334ab82892

--- PageProjet-eMGEqWEV.js
+++
@@ -1,2 +0,0 @@
-import{u as s}from"./historiqueDocuments-B7uSSbzH.js";import{d as r,o as a,a as i,x as u,t as c}from"./index-BmpvvhgS.js";const l=r({__name:"PageProjet",props:{id:{}},setup(e){const o=e,t=s();return a(()=>{t.documentOuvert({id:o.id,à:Date.now()})}),(n,m)=>(i(),u("h1",null,c(n.id),1))}});export{l as default};
-//# sourceMappingURL=PageProjet-eMGEqWEV.js.map
