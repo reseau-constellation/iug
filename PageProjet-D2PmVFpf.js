fd8f56985cffe7475feb591f25b90cae655bbc6f
julienmalard <julienmalard@users.noreply.github.com>
2024-11-18 11:23:16 +0000

    deploy: cea75250375013568503d0b449c7046d40c07474

--- PageProjet-D2PmVFpf.js
+++
@@ -1,2 +0,0 @@
-import{u as s}from"./historiqueDocuments-DBHcAdFo.js";import{d as r,o as a,a as i,x as u,t as c}from"./index-pTpv3qQm.js";const l=r({__name:"PageProjet",props:{id:{}},setup(e){const o=e,t=s();return a(()=>{t.documentOuvert({id:o.id,à:Date.now()})}),(n,m)=>(i(),u("h1",null,c(n.id),1))}});export{l as default};
-//# sourceMappingURL=PageProjet-D2PmVFpf.js.map
