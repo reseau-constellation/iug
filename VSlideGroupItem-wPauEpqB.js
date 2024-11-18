5af307d05133cfbfcd3f63ca7b0421e9f1e43c8a
julienmalard <julienmalard@users.noreply.github.com>
2024-11-18 06:50:25 +0000

    deploy: b450df3a5050c4f7f475a0b5e90457faed4cafd6

---
+++ VSlideGroupItem-wPauEpqB.js
@@ -0,0 +1,2 @@
+import{b9 as o,dk as r,dl as p,dm as u}from"./index-BmpvvhgS.js";const d=o()({name:"VSlideGroupItem",props:p(),emits:{"group:selected":s=>!0},setup(s,t){let{slots:l}=t;const e=r(s,u);return()=>l.default?.({isSelected:e.isSelected.value,select:e.select,toggle:e.toggle,selectedClass:e.selectedClass.value})}});export{d as V};
+//# sourceMappingURL=VSlideGroupItem-wPauEpqB.js.map
