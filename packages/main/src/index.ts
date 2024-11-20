5f03b2be8a67dec57f43f7645e49ff9233e73493
julienmalard <julien.malard@mail.mcgill.ca>
2024-11-20 18:01:04 +0530

    Premier test avec liens profonds

--- packages/main/src/index.ts
+++ packages/main/src/index.ts
@@ -2,6 +2,7 @@ import {app} from 'electron';
 import './security-restrictions';
 import {gestionnaireFenêtres} from '/@/constellation';
 import {restoreOrCreateWindow} from '/@/mainWindow';
+import { electronAppUniversalProtocolClient } from 'electron-app-universal-protocol-client';
 
 /**
  * Prevent electron from running multiple instances.
@@ -46,6 +47,24 @@ app
   .whenReady()
   .then(restoreOrCreateWindow)
   .catch(e => console.error('Failed create window:', e));
+
+app
+  .whenReady()
+  .then(async ()=>{
+    electronAppUniversalProtocolClient.on(
+      'request',
+      async (requestUrl) => {
+        // Handle the request
+  
+        console.log(requestUrl);
+      },
+    );
+  
+    await electronAppUniversalProtocolClient.initialize({
+      protocol: 'your-app-id3',
+      mode: 'development', // Make sure to use 'production' when script is executed in bundled app
+    });
+  });
 
 /**
  * Install Vue.js or any other extension in development mode only.
