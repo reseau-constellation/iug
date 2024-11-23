289eae3e5211853b596017acaf54784a7bfed19b
julienmalard <julienmalard@users.noreply.github.com>
2024-11-23 10:01:25 +0000

    deploy: 7705b1aa12f92979d0afc72fea14430d8f679564

---
+++ VSlideGroupItem-DNjiq0Kr.js
@@ -0,0 +1,2 @@
+import{ba as o,dp as r,dq as p,dr as a}from"./index-B3RH4SGy.js";const d=o()({name:"VSlideGroupItem",props:p(),emits:{"group:selected":s=>!0},setup(s,t){let{slots:l}=t;const e=r(s,a);return()=>l.default?.({isSelected:e.isSelected.value,select:e.select,toggle:e.toggle,selectedClass:e.selectedClass.value})}});export{d as V};
+//# sourceMappingURL=VSlideGroupItem-DNjiq0Kr.js.map
