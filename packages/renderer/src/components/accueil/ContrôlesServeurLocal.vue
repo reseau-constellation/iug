01f6fc71f38163800047827f30b02c0b03449af8
julienmalard <julien.malard@mail.mcgill.ca>
2024-11-24 13:52:59 +0530

    Contrôles serveur

--- packages/renderer/src/components/accueil/ContrôlesServeurLocal.vue
+++ packages/renderer/src/components/accueil/ContrôlesServeurLocal.vue
@@ -24,55 +24,55 @@
       </v-card-item>
       <v-card-text>
         <v-icon start>mdi-information-outline</v-icon>Vous pouvez autoriser l'accès au nœud local afin
-        de pouvoir accéder Constellation à partir d'applications tièrces (par exemple, Python, R). 
-        Ces applications auront accès à votre compte Constellation. Pour des raisons de sécurité, n'approuvez que les applications auxquelles vous faites confiance.
+        de pouvoir accéder Constellation à partir d'applications tièrces. 
+        Voir comment connecter une application dans les langages suivants :
+        
+        <p>
+          <v-chip
+            v-for="langage in langages"
+            :key="langage.nom"
+            class="me-2 mb-2"
+            label
+            variant="outlined"
+            append-icon="mdi-open-in-new"
+            @click="()=>ouvrirLien(langage.lien)"
+          >
+            <template #prepend>
+              <v-icon
+                start
+                :color="langage.couleur"
+              >
+                <component
+                  :is="langage.icône"
+                  class="icon-svg"
+                ></component>
+              </v-icon>
+            </template>
+            {{ t(`communs.langagesInformatiques.${langage.nom}.nom`) }}
+          </v-chip>
+        </p>
         <v-divider />
         <v-list>
+          <v-list-item>
+            Ces applications auront accès à votre compte Constellation. Pour des raisons de sécurité, n'approuvez que les applications auxquelles vous faites confiance.
+          </v-list-item>
           <v-fade-transition>
             <v-list-item v-if="!requêtes?.length">
               <p>
-                Aucune requête en attente d'approbation. Voir comment connecter une application dans les langages suivants :
+                Aucune requête en attente d'approbation. 
               </p>
-              <v-chip
-                class="me-2"
-                label
-                variant="outlined"
-                append-icon="mdi-open-in-new"
-                @click="()=>ouvrirLien('https://docu.réseau-constellation.ca/avancé/autresLangages/python.html')"
-              >
-                Python
-              </v-chip>
-              <v-chip
-                class="me-2"
-                label
-                variant="outlined"
-                append-icon="mdi-open-in-new"
-                @click="()=>ouvrirLien('https://docu.réseau-constellation.ca/avancé/autresLangages/r.html#serveur-existant')"
-              >
-                R
-              </v-chip>
-              <v-chip
-                class="me-2"
-                label
-                variant="outlined"
-                append-icon="mdi-open-in-new"
-                @click="()=>ouvrirLien('https://docu.réseau-constellation.ca/avancé/autresLangages/julia.html')"
-              >
-                Julia
-              </v-chip>
             </v-list-item>
           </v-fade-transition>
-          <v-expand-transition>
-            <v-list-item
-              v-for="r in requêtes"
-              :key="r"
-            >
-              {{ r }}
-              <template #append>
-                
-              </template>
-            </v-list-item>
-          </v-expand-transition>
+          <v-list-item
+            v-for="r in requêtes"
+            :key="r"
+          >
+            {{ r }}
+            <template #append>
+              <v-icon @click="()=>approuverRequête(r)">mdi-check</v-icon>
+              <v-icon @click="()=>refuserRequête(r)">mdi-delete</v-icon>
+            </template>
+          </v-list-item>
         </v-list>
       </v-card-text>
     </v-card>
@@ -81,12 +81,13 @@
 <script setup lang="ts">
 import {suivre} from '@constl/vue';
 import {ref} from 'vue';
+import type {Component} from 'vue';
 import {useDisplay} from 'vuetify';
 
 import {கிளிமூக்கை_பயன்படுத்து} from '@lassi-js/kilimukku-vue';
 import {utiliserServeurLocalConstellation} from '/@/components/utils';
 import {ouvrirLien} from '/@/utils';
-
+import { PythonIcon, RIcon, JuliaIcon, JavaScriptIcon } from 'vue3-simple-icons';
 const {மொழியாக்கம்_பயன்படுத்து} = கிளிமூக்கை_பயன்படுத்து();
 const {$மொ: t} = மொழியாக்கம்_பயன்படுத்து();
 const {mdAndUp} = useDisplay();
@@ -99,5 +100,44 @@ const dialogue = ref(false);
 // Serveur local - statut
 
 // Serveur local - requêtes
-const requêtes = suivre(serveurLocal.suivreRequêtesAuthServeur.bind(serveurLocal));
+const requêtes =  suivre(serveurLocal.suivreRequêtesAuthServeur.bind(serveurLocal));
+const approuverRequête = async (idRequête: string) => {
+  await serveurLocal.approuverRequêteAuthServeur({idRequête});
+};
+const refuserRequête = async (idRequête: string) => {
+  await serveurLocal.refuserRequêteAuthServeur({idRequête});
+};
+
+// Liens clients autres langages
+const langages: {
+  nom: string;
+  couleur: string;
+  icône: Component;
+  lien: string;
+}[] = [
+  {
+    nom: 'py',
+    couleur: '#3776AB',
+    icône: PythonIcon,
+    lien: 'https://docu.réseau-constellation.ca/avancé/autresLangages/python.html',
+  },
+  {
+    nom: 'r',
+    couleur: '#276DC3',
+    icône: RIcon,
+    lien: 'https://docu.réseau-constellation.ca/avancé/autresLangages/r.html#serveur-existant',
+  },
+  {
+    nom: 'julia',
+    couleur: '#9558B2',
+    icône: JuliaIcon,
+    lien: 'https://docu.réseau-constellation.ca/avancé/autresLangages/julia.html',
+  },
+  {
+    nom: 'js',
+    couleur: '#F7DF1E',
+    icône: JavaScriptIcon,
+    lien: 'https://docu.réseau-constellation.ca/avancé/autresLangages/introduction.html',
+  },
+];
 </script>
