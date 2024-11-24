aed28e663ba9bce4e40e25117914799ff9d6b0f1
julienmalard <julienmalard@users.noreply.github.com>
2024-11-24 08:37:13 +0000

    deploy: 673984c6bf03aae731fc653f077f807ffe8a0d19

--- PageProjet-4vQcvcGi.js
+++
@@ -1,2 +0,0 @@
-import{u as s}from"./historiqueDocuments-BHPMm3Uq.js";import{d as r,o as a,a as i,z as u,t as c}from"./index-B3RH4SGy.js";const l=r({__name:"PageProjet",props:{id:{}},setup(e){const o=e,t=s();return a(()=>{t.documentOuvert({id:o.id,à:Date.now()})}),(n,m)=>(i(),u("h1",null,c(n.id),1))}});export{l as default};
-//# sourceMappingURL=PageProjet-4vQcvcGi.js.map
---
+++ VSlideGroupItem-kh9TOa4c.js
@@ -0,0 +1,2 @@
+import{bb as o,dr as r,ds as p,dt as u}from"./index-CqpVgT1a.js";const d=o()({name:"VSlideGroupItem",props:p(),emits:{"group:selected":s=>!0},setup(s,t){let{slots:l}=t;const e=r(s,u);return()=>l.default?.({isSelected:e.isSelected.value,select:e.select,toggle:e.toggle,selectedClass:e.selectedClass.value})}});export{d as V};
+//# sourceMappingURL=VSlideGroupItem-kh9TOa4c.js.map
