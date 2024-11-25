9f60502038b13664ddaad31aab9ec191b09c42ca
julienmalard <julienmalard@users.noreply.github.com>
2024-11-25 06:47:05 +0000

    deploy: 026526dbec07f2d8754ce74e1a78e4f7171457c1

---
+++ PageProjet-CYZsSfwV.js
@@ -0,0 +1,2 @@
+import{u as s}from"./historiqueDocuments-m3X2510u.js";import{d as r,o as a,a as i,C as u,t as c}from"./index-CND5Lrss.js";const l=r({__name:"PageProjet",props:{id:{}},setup(e){const o=e,t=s();return a(()=>{t.documentOuvert({id:o.id,à:Date.now()})}),(n,m)=>(i(),u("h1",null,c(n.id),1))}});export{l as default};
+//# sourceMappingURL=PageProjet-CYZsSfwV.js.map
