6dd941719df8314b002a889da2232ef8bd449774
julienmalard <julienmalard@users.noreply.github.com>
2024-12-05 10:03:55 +0000

    deploy: 181b9a77c81c8ff006238f2b3fa65eced8c1d842

---
+++ PageProjet-DDdp2maE.js
@@ -0,0 +1,2 @@
+import{u as s}from"./historiqueDocuments-CbxrMcFq.js";import{d as r,o as a,a as i,C as u,t as c}from"./index-BNs_NUbA.js";const l=r({__name:"PageProjet",props:{id:{}},setup(e){const o=e,t=s();return a(()=>{t.documentOuvert({id:o.id,à:Date.now()})}),(n,m)=>(i(),u("h1",null,c(n.id),1))}});export{l as default};
+//# sourceMappingURL=PageProjet-DDdp2maE.js.map
---
+++ VSlideGroupItem-DI41ndHm.js
@@ -0,0 +1,2 @@
+import{bh as o,dq as r,dr as p,ds as u}from"./index-BNs_NUbA.js";const d=o()({name:"VSlideGroupItem",props:r(),emits:{"group:selected":s=>!0},setup(s,t){let{slots:l}=t;const e=p(s,u);return()=>l.default?.({isSelected:e.isSelected.value,select:e.select,toggle:e.toggle,selectedClass:e.selectedClass.value})}});export{d as V};
+//# sourceMappingURL=VSlideGroupItem-DI41ndHm.js.map
