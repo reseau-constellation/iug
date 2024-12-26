c3f6e103dab613067be1d1612fa5c660c00f2d94
julienmalard <julien.malard@mail.mcgill.ca>
2024-12-26 13:19:59 +0530

    Présentation

--- packages/renderer/src/components/tableaux/SélecteurTableau.vue
+++ packages/renderer/src/components/tableaux/SélecteurTableau.vue
@@ -4,31 +4,40 @@
     :multiples="false"
     @selectionnee="bds => (bdChoisie = bds[0])"
   />
-  <v-select
-    v-model="idTableauSélectionné"
-    :loading="!tableaux"
-    :disabled="!tableaux"
-    :items="tableaux"
-    item-value="id"
-    item-title="id"
-  >
-    <template #selection="{item}">
-      <jeton-tableau :id="item.value" />
-    </template>
-    <template #item="{item}">
-      <item-tableau :id="item.value" />
-    </template>
-  </v-select>
+  <v-expand-transition>
+    <v-select
+      v-show="bdFinale"
+      v-model="idTableauSélectionné"
+      class="mt-4"
+      :loading="!tableaux"
+      :disabled="!tableaux"
+      :items="tableaux"
+      :label="t('tableaux.sélection.étiquette')"
+      item-value="id"
+      variant="outlined"
+    >
+      <template #selection="{item}">
+        <jeton-tableau :id="item.value" />
+      </template>
+      <template #item="{item, props: propsItem}">
+        <item-tableau
+          v-bind="Object.fromEntries(Object.entries(propsItem).filter(([c, v]) => c !== 'title'))"
+          :id="item.value"
+        />
+      </template>
+    </v-select>
+  </v-expand-transition>
 </template>
 <script setup lang="ts">
 import {suivre} from '@constl/vue';
-import {computed, ref, watchEffect} from 'vue';
+import {computed, ref, watch, watchEffect} from 'vue';
 
 import ItemTableau from './ItemTableau.vue';
 import JetonTableau from './JetonTableau.vue';
 import SelecteurBd from '/@/components/bds/SélecteurBd.vue';
 
 import {utiliserConstellation} from '../utils';
+import { கிளிமூக்கை_பயன்படுத்து } from '@lassi-js/kilimukku-vue';
 
 const props = defineProps<{idBd?: string}>();
 const émettre = defineEmits<{
@@ -36,6 +45,9 @@ const émettre = defineEmits<{
 }>();
 
 const constl = utiliserConstellation();
+
+const {மொழியாக்கம்_பயன்படுத்து} = கிளிமூக்கை_பயன்படுத்து();
+const {$மொ: t} = மொழியாக்கம்_பயன்படுத்து();
 
 // Bd
 const bdChoisie = ref<string>();
@@ -51,4 +63,6 @@ const idTableauSélectionné = ref<string>();
 watchEffect(() => {
   émettre('selectionne', idTableauSélectionné.value);
 });
+
+watch(bdFinale, ()=> idTableauSélectionné.value = undefined);
 </script>
