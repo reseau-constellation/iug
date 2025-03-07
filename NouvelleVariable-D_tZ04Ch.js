642ffcb4b91984aa5a29d95c9ffe1a6365801d85
julienmalard <julienmalard@users.noreply.github.com>
2025-03-07 07:49:55 +0000

    deploy: 0255d030fb9f33a02d9499074baf8b37e7ec5fa6

---
+++ NouvelleVariable-D_tZ04Ch.js
@@ -0,0 +1,2 @@
+import{_ as m}from"./NouvelleVariable.vue_vue_type_script_setup_true_lang-xSBmTyDQ.js";import"./index-MnnZi1EE.js";export{m as default};
+//# sourceMappingURL=NouvelleVariable-D_tZ04Ch.js.map
---
+++ PageProjet-Gn_xtB-r.js
@@ -0,0 +1,2 @@
+import{u as s}from"./historiqueDocuments-BsPkwq2Y.js";import{d as r,o as a,A as i,a as u,t as c}from"./index-MnnZi1EE.js";const l=r({__name:"PageProjet",props:{id:{}},setup(e){const o=e,t=s();return a(()=>{t.documentOuvert({id:o.id,à:Date.now()})}),(n,m)=>(u(),i("h1",null,c(n.id),1))}});export{l as default};
+//# sourceMappingURL=PageProjet-Gn_xtB-r.js.map
