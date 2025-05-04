41b1adeb7da01cdba86e91990baf1296ef1d1d34
julienmalard <julienmalard@users.noreply.github.com>
2025-05-04 11:32:47 +0000

    deploy: e0b435f8a8762e37990a750d271dc3fbcdabb6d4

--- VSlideGroupItem-v-bmPAIh.js
+++
@@ -1,2 +0,0 @@
-import{ba as o,da as r,db as a,dc as p}from"./index-BRylzrLF.js";const d=o()({name:"VSlideGroupItem",props:r(),emits:{"group:selected":s=>!0},setup(s,t){let{slots:l}=t;const e=a(s,p);return()=>l.default?.({isSelected:e.isSelected.value,select:e.select,toggle:e.toggle,selectedClass:e.selectedClass.value})}});export{d as V};
-//# sourceMappingURL=VSlideGroupItem-v-bmPAIh.js.map
